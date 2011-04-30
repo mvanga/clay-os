5c74ce818dbda176541ea3a90d77f41c7cabf70e
Manohar Vanga <manohar.vanga@gmail.com>
2011-04-30 22:41:27 +0200

    Made msecs_to_jiffies and jiffies_to_msecs into macros
    Signed-off-by: Manohar Vanga <manohar.vanga@gmail.com>

--- include/clay/jiffies.h
+++ include/clay/jiffies.h
@@ -22,15 +22,11 @@ extern volatile u32 jiffies;
 #define time_after_eq(a,b)  ((long)(a) - (long)(b) >= 0)
 #define time_before_eq(a,b) time_after_eq(b,a)
 
-u32 msecs_to_jiffies(const u32 m)
-{
-	return (m + (1000 / HZ) - 1) / (1000 / HZ);
-}
+#define msecs_to_jiffies(x) \
+	((x + (1000 / HZ) - 1) / (1000 / HZ))
 
-u32 jiffies_to_msecs(const u32 j)
-{
-	return (1000 / HZ) * j;
-}
+#define jiffies_to_msecs(x) \
+	((1000 / HZ) * x)
 
 void delay(int msec);
 
